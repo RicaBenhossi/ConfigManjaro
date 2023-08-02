b72439c90c84f475092d9d1b58c5073ca03bb46a
Luis Ricardo Benhossi <ricardo.benhossi@gmail.com>
2023-08-01 22:08:39 -0300

    Adjust on configs and add a new file.

--- run_to_configure_pc.zsh
+++ run_to_configure_pc.zsh
@@ -7,7 +7,7 @@ echo "*                                                         *"
 echo "***********************************************************"
 echo
 # Variables
-config_folder="/home/$USER/MyConfigs/Configs"
+config_folder="/home/$USER/MyConfigss/Configs"
 
 echo "====================> Setting the fastest mirror (can take a while)"
 echo
@@ -24,18 +24,18 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf &&
 echo
 echo "====================> Configure ZSH"
 echo
-echo "--------------------> Installing fonts"
-echo
-echo 'Press any key to continue...'; read -k1 -s
-echo
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -P ~/.local/share/fonts &&
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -P ~/.local/share/fonts &&
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -P ~/.local/share/fonts &&
-wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -P ~/.local/share/fonts
-echo
+# echo "--------------------> Installing fonts"
+# echo
+# echo 'Press any key to continue...'; read -k1 -s
+# echo
+# wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -P ~/.local/share/fonts &&
+# wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf -P ~/.local/share/fonts &&
+# wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf -P ~/.local/share/fonts &&
+# wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf -P ~/.local/share/fonts
+# echo
 echo "--------------------> Configure files for zsh"
 echo
-echo "*** It will ask for Gihub\'s username and password or token."
+echo "*** It will ask for Github\'s username and password or token."
 echo "The token is in LastPass."
 echo
 cd ~ &&
@@ -43,13 +43,21 @@ rm -f ~/.zshrc;
 rm -f ~/.sh_aliases;
 # rm -f ~/.p10k.zsh;
 mkdir -p ~/.config/nvim;
-ln -s $config_folder/.zshrc ~/.zshrc_starship;
+ln -s $config_folder/.zshrc_starship ~/.zshrc;
 ln -s $config_folder/.sh_aliases ~/.sh_aliases;
 # ln -s $config_folder/.p10k.zsh ~/.p10k.zsh;
 ln -s $config_folder/init.vim ~/.config/nvim/init.vim;
+ln -s $config_folder/starship.toml ~/.config/starship.toml
+echo
+echo "====================> Configure Terminal Visual"
+echo
+echo 'Press any key to continue...'; read -k1 -s
+echo
+rm ~/.config/gtk-3.0/gtk.css &&
+ln -s ~/MyConfigss/Configs/gtk-3.0-gtk.css ~/.config/gtk-3.0/gtk.css
 echo
 echo "====================> Download ssh keys"
-echo
+echoasdf
 echo 'Copy your ssh keys and store them in ~/.ssh. Press any key to continue...'; read -k1 -s
 echo
 chmod 600 ~/.ssh/id_ed25519 &&
@@ -76,16 +84,16 @@ echo "--------------------> fast-syntax-highlighting"
 echo
 echo 'Press any key to continue...'; read -k1 -s
 echo
-mkdir -p ~/.config/zsh/zsh-plugins &&
+mkdir -p ~/.config/zsh/plugins &&
 git clone git@github.com:zdharma/fast-syntax-highlighting.git ~/.config/zsh/plugins/fast-syntax-highlighting &&
-ln $config_folder/clean_my.ini /.config/zsh/plugins/fast-syntax-highlighting/themes &&
+ln $config_folder/clean_my.ini ~/.config/zsh/plugins/fast-syntax-highlighting/themes &&
 source ~/.zshrc
 fast-theme clean_my
 echo
 echo "====================> Load gnome-terminal configs"
 echo
 echo 'Press any key to continue...'; read -k1 -s
-dconf load /org/gnome/terminal/legacy/profiles:/ < ~/MyConfig/Configs/gnome-terminal-profiles.dconf
+dconf load /org/gnome/terminal/legacy/profiles:/ < ~/MyConfigs/gnome-extensions/gnome-terminal-profiles.dconf
 echo
 echo "====================> Update the system"
 echo
@@ -97,7 +105,7 @@ echo "====================> Install Starship terminal"
 echo
 echo 'Press any key to continue...'; read -k1 -s
 echo
-pacman -S starship
+sudo pacman -Sy starship
 echo
 echo "====================> Install tree to terminal"
 echo
@@ -105,7 +113,7 @@ echo 'Press any key to continue...'; read -k1 -s
 echo
 pamac install tree --no-confirm &&
 echo
-echo "====================> Install Make"
+echo "====================> Install Neovim"
 echo
 echo 'Press any key to continue...'; read -k1 -s
 echo
@@ -115,13 +123,13 @@ echo "====================> Install pkg-config"
 echo
 echo 'Press any key to continue...'; read -k1 -s
 echo
-pacman -Sy pkg-config &&
+sudo pacman -Sy pkg-config &&
 echo
-echo "====================> Install Neovim"
+echo "====================> Install Make"
 echo
 echo 'Press any key to continue...'; read -k1 -s
 echo
-pacman -Sy make &&
+sudo pacman -Sy make &&
 echo
 echo
 read -rsp $"Configuration process is finished. Press any key to proceed..."
